982e9aaef3c97a52c33d75cadb29a72e09d34082
Oleh Kakherskyi <olehkakherskiy@gmail.com>
2016-08-07 19:25:44 +0300

    javadoc is added to exception hierarchy and message formatter

--- src/main/java/com/epam/project4/exception/DaoException.java
+++ src/main/java/com/epam/project4/exception/DaoException.java
@@ -1,26 +1,31 @@
 package main.java.com.epam.project4.exception;
 
 /**
+ * Exception indicates that some exception was caused during processing
+ * {@link main.java.com.epam.project4.model.dao.GenericDao} and it's subclasses
+ * operations
+ *
  * @author Oleh Kakherskyi (olehkakherskiy@gmail.com)
+ * @see main.java.com.epam.project4.model.dao.GenericDao
  */
 public class DaoException extends Exception {
 
+    /**
+     * {@inheritDoc}
+     */
     public DaoException() {
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @param message {@inheritDoc}
+     */
     public DaoException(String message) {
         super(message);
     }
 
     public DaoException(String message, Throwable cause) {
         super(message, cause);
-    }
-
-    public DaoException(Throwable cause) {
-        super(cause);
-    }
-
-    public DaoException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
